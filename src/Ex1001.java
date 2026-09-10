08b81e995d85700ca53b253113d93a1a973b9819
Gustavo <kazuharogu@gmail.com>
2026-09-10 03:32:01 -0300

    teste

--- src/Ex1001.java
+++ src/Ex1001.java
@@ -1,4 +1,17 @@
 package src;
+import java.util.Scanner;
 
 public class Ex1001 {
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int X = A + B;
+
+        System.out.println("X = " + X);
+
+        sc.close();
+        }
+    }
